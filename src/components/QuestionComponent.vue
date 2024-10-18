b09b2fcddf9d389f4a7cb5c07138be9ef6c91ebc
IraEiza <iratze.eizaguirre@gmail.com>
2024-10-18 19:58:17 +0100

    Question card "styled"

--- src/components/QuestionComponent.vue
+++ src/components/QuestionComponent.vue
@@ -1,23 +1,28 @@
 <template>
-    <div class="p-4 bg-gray-100 rounded-md shadow-md">
+  <div class="p-10 h-screen bg-cover bg-center flex justify-center items-center">
+    <div class="p-4 bg-red-500 bg-opacity-60 rounded-xl shadow-md text-white text-center">
       <div v-if="currentQuestion">
-        <div class="text-xl font-bold mb-4">{{ currentQuestion.question }}</div>
-        <div class="flex flex-col gap-4">
+        <div class="text-4xl font-bold mb-6">{{ currentQuestion.question }}</div>
+
+        <!-- Respuestas en formato 2x2 con estilo unificado -->
+        <div class="grid grid-cols-2 gap-4">
           <button
             v-for="(answer, index) in currentQuestion.answers"
             :key="index"
             @click="selectAnswer(index)"
-            class="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+            class="p-4 bg-pink-500 text-white font-bold rounded-md shadow-lg hover:bg-pink-400 hover:text-yellow-200 transition-all duration-300 ease-in-out transform hover:scale-110 focus:outline-none cursor-pointer"
           >
             {{ answer }}
           </button>
         </div>
       </div>
       <div v-else>
-        <div class="text-center text-2xl">¡Has completado todas las preguntas!</div>
+        <div class="text-3xl font-bold">¡Has completado todas las preguntas!</div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
+
   
   <script setup>
   import { ref, watch } from 'vue'
